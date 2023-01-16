4525bd80dcda33106aaa8fe301e081e79b8dcef0
Cuauhtli <cuau_daali@hotmail.com>
2023-01-16 15:59:47 -0600

    adding .env

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,9 +1,19 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "../../service-account-file.json";
 
 if (admin.apps.length == 0) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: process.env.type,
+      project_id: process.env.project_id,
+      private_key_id: process.env.private_key_id,
+      private_key: process.env.private_key,
+      client_email: process.env.client_email,
+      client_id: process.env.client_id,
+      auth_uri: process.env.auth_uri,
+      token_uri: process.env.token_uri,
+      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+      client_x509_cert_url: process.env.client_x509_cert_url,
+    }),
     databaseURL: "https://tmdb-b6b20-default-rtdb.firebaseio.com",
   });
   admin.firestore().settings({ ignoreUndefinedProperties: true });
