3305d7480505445f229a1c69fac2ecc9f037afc9
Cuauhtli <cuau_daali@hotmail.com>
2023-01-16 17:29:59 -0600

    Change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    VERCEL_URL: process.env.VERCEL_URL,
+  },
 };
 
 module.exports = nextConfig;
