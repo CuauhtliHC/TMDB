ceae66bc228bbbde08071ca41f6c53b57d032f6b
Cuauhtli <cuau_daali@hotmail.com>
2023-01-16 18:10:15 -0600

    Fix .env

--- utils/db/index.js
+++ utils/db/index.js
@@ -12,7 +12,7 @@ if (admin.apps.length == 0) {
       client_email: process.env.CLIENT_EMAIL,
       client_id: process.env.CLIENT_ID,
       auth_uri: process.env.AUTH_URI,
-      token_uri: process.env.TOKE_URI,
+      token_uri: process.env.TOKEN_URI,
       auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
       client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
     }),
